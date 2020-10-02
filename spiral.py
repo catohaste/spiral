393932b2b56dfba46995539db72e364b39a5e3db
catohaste <catohaste@users.noreply.github.com>
2020-10-02 21:57:17 +0100

    One prong, to 4 prongs on 4 spirals.

--- spiral.py
+++ spiral.py
@@ -8,31 +8,68 @@ from matplotlib.animation import FuncAnimation
 #			start_position
 #			direction
 #			length
-#			angular_v
-#			forward_v
 
-num_points_in_ring = 10
+animation_duration = 3000
+
+num_points_in_ring = 50
 radius = 1
 
-test_plot = plt.figure()
-ax = test_plot.add_subplot(111)
-test_prong, = ax.plot([],[])
+spiralN = 4
 
+prongN = 4
+prongs_list = []
+centre_of_mass_list = []
+
+fig = plt.figure(figsize=(12,4))
+axs = {}
+for spiral in range(spiralN):
+    axs[spiral] = fig.add_subplot(1,spiralN,spiral+1)
+
+    for idx in range(prongN):
+        line, = axs[spiral].plot([],[])
+        prongs_list.append(line)
+    
+    mass, = axs[spiral].plot([],[],'-ok',markersize=10)
+    centre_of_mass_list.append(mass)
+
+prongs = tuple(prongs_list)
+centre_of_mass = tuple(centre_of_mass_list)
+    
 def init_animation():
-	ax.set_xlim([-2,2])
-	ax.set_ylim([-2,2])
-	ax.set_aspect('equal')
-	return test_prong,
+    for spiral in range(spiralN):
+	    axs[spiral].set_xlim([-1.5,1.5])
+	    axs[spiral].set_ylim([-1.5,1.5])
+	    axs[spiral].set_aspect('equal')
+    
+    return prongs + centre_of_mass
 
 def update(frame):
-	theta = frame
-	x = radius * np.cos(theta)
-	y = radius * np.sin(theta)
-	test_prong.set_data([0,x], [0,y])
-	return test_prong,
+    theta = [frame, frame + np.pi/2, frame + np.pi, frame + (3*np.pi)/2]
+    direction = np.asarray([[1,1,1,1],
+                    [1,1,1,-1],
+                    [1,1,-1,-1],
+                    [1,-1,1,-1]])
+                    
+    x = np.ndarray(direction.shape)
+    y = np.ndarray(direction.shape)
+    for spiral in range(spiralN):
+        x[spiral,:] = radius * np.cos(np.multiply(direction[spiral,:],theta))
+        y[spiral,:] = radius * np.sin(np.multiply(direction[spiral,:],theta))
+        
+    avg_x = np.mean(x,axis=1)
+    avg_y = np.mean(y,axis=1)
+    
+    x = x.flatten()
+    y = y.flatten()
+    
+    for idx, prong in enumerate(prongs):
+        prong.set_data([0,x[idx]], [0,y[idx]])
+    for spiral in range(spiralN):
+        centre_of_mass[spiral].set_data([avg_x[spiral]], [avg_y[spiral]])
+    return prongs + centre_of_mass
 	
-ani = FuncAnimation(test_plot, update, frames=np.linspace(0,2*np.pi,num_points_in_ring), interval=100,
-                    init_func=init_animation, blit=True)
+ani = FuncAnimation(fig, update, frames=np.linspace(0,2*np.pi,num_points_in_ring), interval = animation_duration / (num_points_in_ring - 1),
+                    init_func = init_animation, blit = True)
 
 plt.show()
 
