3f8356f5ea60af840561b5300092429c344021c4
catohaste <catohaste@users.noreply.github.com>
2020-10-05 10:22:26 +0100

    Write animation to file.

--- spiral.py
+++ spiral.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
 
 # define spiral
 #	has 4 prongs
@@ -18,21 +19,26 @@ spiralN = 4
 
 prongN = 4
 prongs_list = []
+points_list = []
 centre_of_mass_list = []
 
 fig = plt.figure(figsize=(12,4))
 axs = {}
+colors = ['C0', 'C1', 'C2', 'C3']
 for spiral in range(spiralN):
     axs[spiral] = fig.add_subplot(1,spiralN,spiral+1)
 
     for idx in range(prongN):
-        line, = axs[spiral].plot([],[])
+        line, = axs[spiral].plot([],[], color = colors[idx])
+        point, = axs[spiral].plot([],[], '-o', markersize=5, color = colors[idx])
         prongs_list.append(line)
+        points_list.append(point)
 
-    mass, = axs[spiral].plot([],[],'-ok',markersize=10)
+    mass, = axs[spiral].plot([],[],'ok',markersize=10)
     centre_of_mass_list.append(mass)
 
 prongs = tuple(prongs_list)
+points = tuple(points_list)
 centre_of_mass = tuple(centre_of_mass_list)
 
 def init_animation():
@@ -41,7 +47,7 @@ def init_animation():
         axs[spiral].set_ylim([-1.5,1.5])
         axs[spiral].set_aspect('equal')
         axs[spiral].axis('off')
-    return prongs + centre_of_mass
+    return prongs + points + centre_of_mass
 
 def update(frame):
     theta = [frame, frame + np.pi/2, frame + np.pi, frame + (3*np.pi)/2]
@@ -61,13 +67,21 @@ def update(frame):
 
     for idx, prong in enumerate(prongs):
         prong.set_data([0,x[idx]], [0,y[idx]])
+        points[idx].set_data([x[idx]], [y[idx]])
     
     for spiral in range(spiralN):
         centre_of_mass[spiral].set_data([avg_x[spiral]], [avg_y[spiral]])
+        
+    return prongs + points + centre_of_mass
     
-    return prongs + centre_of_mass
+plt.tight_layout()
 
 ani = FuncAnimation(fig, update, frames=np.linspace(0,2*np.pi,num_points_in_ring), interval = animation_duration / (num_points_in_ring - 1), init_func = init_animation, blit = True)
 
 plt.show()
+
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=24)
+
+ani.save('animation.mp4', writer=writer)
 
