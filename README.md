4fd5176d8c5de82854150f1496819d74f88a947c
catohaste <catohaste@users.noreply.github.com>
2022-02-19 17:32:10 +0000

    Add spiral permutations plot and output.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ![spiral animation](https://github.com/catohaste/spiral/blob/master/animation.gif?raw=true)
+
+![spiral permutations](https://github.com/catohaste/spiral/blob/master/permutation.jpg?raw=true)
---
+++ permutation.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+num_points_in_ring = 100
+radius = 1
+
+spiralN = 4
+
+prongN = 4
+prongs_list = []
+points_list = []
+centre_of_mass_list = []
+
+# fig = plt.figure(figsize=(6,4))
+fig = plt.figure(figsize=(12,3))
+
+axs = ImageGrid(fig, (0,0,1,1),
+                 nrows_ncols=(1, 4),  # creates 2x2 grid of axes
+                 axes_pad=0,  # pad between axes in inch.
+                 share_all=True
+                 )
+                 
+axs[0].get_yaxis().set_ticks([])
+axs[0].get_xaxis().set_ticks([])
+
+# axs = {}
+colors = ['C0', 'C1', 'C2', 'C3']
+
+# draw circle
+circle = plt.Circle((0, 0), radius, facecolor='none', edgecolor='black', linewidth=3)
+
+for spiral in range(spiralN):
+    
+    theta_zero = [0, np.pi/2, np.pi, (3*np.pi)/2]
+    direction = np.asarray([[1,1,1,1], [1,1,1,-1], [1,1,-1,-1], [1,-1,1,-1]])
+    
+    circle = plt.Circle((0, 0), radius, facecolor='none', edgecolor='black', linewidth=3)
+    axs[spiral].add_patch(circle)
+    
+    theta_zero = [0, np.pi/2, np.pi, (3*np.pi)/2]
+    direction = np.asarray([[1,1,1,1], [1,1,1,-1], [1,1,-1,-1], [1,-1,1,-1]])
+        
+    for point_idx, theta in enumerate(theta_zero):
+        x = radius * np.cos(theta)
+        y = radius * np.sin(theta)
+        
+        c = direction[spiral, point_idx]
+        if c > 0:
+            color = 'C0'
+        else:
+            color = 'C1'            
+        mass, = axs[spiral].plot(x,y,'o',markersize=15, c=color)
+    
+    axs[spiral].set_xlim([-1.5,1.5])
+    axs[spiral].set_ylim([-1.5,1.5])
+    axs[spiral].set_aspect('equal')
+    axs[spiral].axis('off')
+    
+plt.show()
+
+fig.savefig("permutation.jpg")
